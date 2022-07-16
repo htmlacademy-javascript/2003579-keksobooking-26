71b5afa91845ec4f80064ec1df90465ce721d401
baranki-gnu <98429714+baranki-gnu@users.noreply.github.com>
2022-07-16 18:49:12 +0300

    Валидация формы

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 import {similarObjects} from './data.js';
 import {similarElements} from './generator.js';
-import {switchFormOnOff} from './forms.js';
+import {switchFormOnOff} from './forms-toggle.js';
+import {pristine} from './forms-validation.js';
